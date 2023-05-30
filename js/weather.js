614ca1b0185d51809938055d07f27335afd6a7aa
Ahyeon Lee <laah0602@gmail.com>
2023-05-30 23:47:52 +0900

    [ADD] final weather code

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,13 @@ const API_KEY = "a3ea0734507a5a126b9538b52eeb1746";
 function onGeoSuccess(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat, lon);
-    const url = `https://openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you.");
